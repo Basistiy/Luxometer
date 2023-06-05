faea1f95ecaa1896245e6b7cc580d3a0a0578e8c
Evgeny <zzzzbh@gmail.com>
2023-06-05 13:05:20 +0100

    Added camera selection option

--- Sources/Luxometer/Luxometer.swift
+++ Sources/Luxometer/Luxometer.swift
@@ -2,67 +2,191 @@ import Foundation
 import AVKit
 
 
-@available(iOS 11.1, *)
+@available(iOS 13, *)
 public class Luxometer : NSObject, AVCaptureVideoDataOutputSampleBufferDelegate{
-    public var capturedIlluminance : ((Int) -> ())?
-    private var permissionGranted = false
-    private let captureSession = AVCaptureSession()
+    public var capturedIlluminance : ((Int, String) -> ())?
+    public var calibrationConstant = 70.0
+    @objc dynamic var videoDeviceInput: AVCaptureDeviceInput!
+    private let session = AVCaptureSession()
     private let sessionQueue = DispatchQueue(label: "sessionQueue")
+    private enum SessionSetupResult {
+        case success
+        case notAuthorized
+        case configurationFailed
+    }
+    private var cameraUsed = ""
+    private var setupResult: SessionSetupResult = .success
+    
+    override init() {
+        super.init()
+        configureSession()
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            // The user has previously granted access to the camera.
+            break
+            
+        case .notDetermined:
+            /*
+             The user has not yet been presented with the option to grant
+             video access. Suspend the session queue to delay session
+             setup until the access request has completed.
+             
+             Note that audio access will be implicitly requested when we
+             create an AVCaptureDeviceInput for audio during session setup.
+             */
+            sessionQueue.suspend()
+            AVCaptureDevice.requestAccess(for: .video, completionHandler: { granted in
+                if !granted {
+                    self.setupResult = .notAuthorized
+                }
+                self.sessionQueue.resume()
+            })
+            
+        default:
+            // The user has previously denied access.
+            setupResult = .notAuthorized
+        }
+        
+    }
 
 
     public func startMeasurement(){
-        checkPermission()
-        sessionQueue.async { [unowned self] in
-            self.setupCaptureSession()
-            self.captureSession.startRunning()
+        sessionQueue.async {
+            self.session.startRunning()
         }
         
     }
     
     public func stopMeasurement(){
-        sessionQueue.async { [unowned self] in
-            self.captureSession.stopRunning()
+        sessionQueue.async {
+            self.session.stopRunning()
         }
         
     }
     
-    private func checkPermission() {
-        switch AVCaptureDevice.authorizationStatus(for: .video) {
-        case .authorized:
-            permissionGranted = true
-            
-        case .notDetermined:
-            requestPermission()
-            
-        default:
-            permissionGranted = false
-        }
-    }
     
-    
-    private func setupCaptureSession() {
-        let videoOutput = AVCaptureVideoDataOutput()
-        guard permissionGranted else {
-            print("Luxometer does not have camera permission")
+    private func configureSession() {
+        if setupResult != .success {
             return
-            
         }
-        let discovery = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .back)
-        print(discovery.devices)
-        guard let videoDevice = AVCaptureDevice.default(.builtInTrueDepthCamera,for: .video, position: .front) else {
-            print("Luxometer cannot get access to the built in True Depth front camera")
-            return }
-        guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice) else { return }
-        guard captureSession.canAddInput(videoDeviceInput) else { return }
-        captureSession.addInput(videoDeviceInput)
+        
+        session.beginConfiguration()
+        
+        /*
+         Do not create an AVCaptureMovieFileOutput when setting up the session because
+         Live Photo is not supported when AVCaptureMovieFileOutput is added to the session.
+         */
+        session.sessionPreset = .photo
+        
+        // Add video input.
+        do {
+            var defaultVideoDevice: AVCaptureDevice?
+            
+            // Choose the back dual camera, if available, otherwise default to a wide angle camera.
+            
+            if let dualCameraDevice = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+                defaultVideoDevice = dualCameraDevice
+            } else if let dualWideCameraDevice = AVCaptureDevice.default(.builtInDualWideCamera, for: .video, position: .back) {
+                // If a rear dual camera is not available, default to the rear dual wide camera.
+                defaultVideoDevice = dualWideCameraDevice
+            } else if let backCameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+                // If a rear dual wide camera is not available, default to the rear wide angle camera.
+                defaultVideoDevice = backCameraDevice
+            } else if let frontCameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) {
+                // If the rear wide angle camera isn't available, default to the front wide angle camera.
+                defaultVideoDevice = frontCameraDevice
+            }
+            guard let videoDevice = defaultVideoDevice else {
+                print("Default video device is unavailable.")
+                setupResult = .configurationFailed
+                session.commitConfiguration()
+                return
+            }
+            self.cameraUsed = videoDevice.localizedName
+            let videoDeviceInput = try AVCaptureDeviceInput(device: videoDevice)
+            
+            if session.canAddInput(videoDeviceInput) {
+                session.addInput(videoDeviceInput)
+                self.videoDeviceInput = videoDeviceInput
+                
+            } else {
+                print("Couldn't add video device input to the session.")
+                setupResult = .configurationFailed
+                session.commitConfiguration()
+                return
+            }
+        } catch {
+            print("Couldn't create video device input: \(error)")
+            setupResult = .configurationFailed
+            session.commitConfiguration()
+            return
+        }
+
+        
+        // Add the photo output.
+        let videoOutput = AVCaptureVideoDataOutput()
         videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "sampleBufferQueue"))
-        captureSession.addOutput(videoOutput)
-        videoOutput.connection(with: .video)?.videoOrientation = .portrait
+        if session.canAddOutput(videoOutput) {
+            session.addOutput(videoOutput)
+            videoOutput.connection(with: .video)?.videoOrientation = .portrait
+                        
+        } else {
+            print("Could not add photo output to the session")
+            setupResult = .configurationFailed
+            session.commitConfiguration()
+            return
+        }
+        
+        session.commitConfiguration()
     }
     
-    private func requestPermission() {
-        AVCaptureDevice.requestAccess(for: .video) { [unowned self] granted in
-            self.permissionGranted = granted
+    // Changes camera used to capture illuminance
+    public func changeCamera(){
+        sessionQueue.async {
+            let currentVideoDevice = self.videoDeviceInput.device
+            let currentPosition = currentVideoDevice.position
+
+            let backVideoDeviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera, .builtInDualWideCamera, .builtInWideAngleCamera],
+                                                                                   mediaType: .video, position: .back)
+            let frontVideoDeviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInTrueDepthCamera, .builtInWideAngleCamera],
+                                                                                    mediaType: .video, position: .front)
+            var newVideoDevice: AVCaptureDevice? = nil
+            
+            switch currentPosition {
+            case .unspecified, .front:
+                newVideoDevice = backVideoDeviceDiscoverySession.devices.first
+                
+            case .back:
+                newVideoDevice = frontVideoDeviceDiscoverySession.devices.first
+                
+            @unknown default:
+                print("Unknown capture position. Defaulting to back, dual-camera.")
+                newVideoDevice = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back)
+            }
+            
+            if let videoDevice = newVideoDevice {
+                do {
+                    let videoDeviceInput = try AVCaptureDeviceInput(device: videoDevice)
+                    
+                    self.session.beginConfiguration()
+                    self.session.removeInput(self.videoDeviceInput)
+                    
+                    if self.session.canAddInput(videoDeviceInput) {
+                        self.session.addInput(videoDeviceInput)
+                        self.videoDeviceInput = videoDeviceInput
+                    } else {
+                        self.session.addInput(self.videoDeviceInput)
+                    }
+                    self.session.commitConfiguration()
+                    DispatchQueue.main.async {
+                        self.cameraUsed = videoDevice.localizedName
+                    }
+                } catch {
+                    print("Error occurred while creating video device input: \(error)")
+                }
+            }
+            
+
         }
     }
     
@@ -71,9 +195,9 @@ public class Luxometer : NSObject, AVCaptureVideoDataOutputSampleBufferDelegate{
         let metadata = CFDictionaryCreateMutableCopy(nil, 0, rawMetadata) as NSMutableDictionary
         let exifData = metadata.value(forKey: "{Exif}") as? NSMutableDictionary
         let brightness : Double = exifData?["BrightnessValue"] as! Double
-        let illuminance = Int(70*pow(2, brightness))
+        let illuminance = Int(calibrationConstant*pow(2, brightness))
         if let captured = self.capturedIlluminance{
-            captured(illuminance)
+            captured(illuminance, cameraUsed)
         }
         else{
             print("Illuminance is \(illuminance) lux. Set capturedIlluminance to your Luxometer instance to get the value and avoid this message.")
